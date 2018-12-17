f80bba8edd19cf13a21bcff598fdf6d6bc4c4d07
Md Inzamamul Haque <inzamamul.haque@relevancelab.com>
2018-12-17 18:11:58 +0530

    added 'notifies' property to resources

--- recipes/install_java.rb
+++ recipes/install_java.rb
@@ -5,13 +5,15 @@ apt_update 'update'
 apt_package 'default-jdk' do
   action :install
   options '-y'
+  notifies :run, 'ruby_block[Set JAVA_HOME]', :immediately
 end
 
-ruby_block 'Set JAVA_HOME in /etc/environment' do
+ruby_block 'Set JAVA_HOME' do
     block do
       file = Chef::Util::FileEdit.new('/etc/environment')
       file.insert_line_if_no_match(/^JAVA_HOME=/, "JAVA_HOME=#{node['java']['java_home']}")
       file.search_file_replace_line(/^JAVA_HOME=/, "JAVA_HOME=#{node['java']['java_home']}")
       file.write_file
     end
+    action :nothing
   end
--- recipes/install_tomcat.rb
+++ recipes/install_tomcat.rb
@@ -4,13 +4,15 @@ tomcat_install_dir=node['tomcat_v9']['tomcat_install_dir']
 tomcat_user=node['tomcat_v9']['tomcat_user']
 java_home=node['java']['java_home']
 
-group tomcat_user
+apt_update 'update'
 
 directory tomcat_install_dir do
   mode '0755'
   recursive true
   action :create
 end
+
+group tomcat_user
 
 user tomcat_user do
   group tomcat_user
@@ -21,19 +23,21 @@ end
 
 tar_extract "#{tomcat_url}v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz" do
   target_dir tomcat_install_dir
-  creates "#{tomcat_install_dir}/conf"
   tar_flags [ '-P', '--strip-components 1' ]
+  not_if { ::File.exist?("#{tomcat_install_dir}/conf") }
+  notifies :run, 'script[change_permissions]', :immediately
 end
 
-script 'change required permissions' do
+script 'change_permissions' do
   interpreter 'bash'
   cwd tomcat_install_dir
   code <<-EOH
-          chgrp -R tomcat /opt/tomcat
+          chgrp -R #{tomcat_user} #{tomcat_install_dir}
           chmod -R g+r conf
           chmod g+x conf
-          chown -R tomcat webapps/ work/ temp/ logs/
+          chown -R #{tomcat_user} webapps/ work/ temp/ logs/
           EOH
+  action :nothing
 end
 
 template '/etc/systemd/system/tomcat.service' do
@@ -43,11 +47,12 @@ template '/etc/systemd/system/tomcat.service' do
      :tomcat_install_dir => tomcat_install_dir,
      :tomcat_user => tomcat_user
    )
+  notifies :run, 'execute[daemon-reload]', :immediately
 end
 
 execute 'daemon-reload' do
   command "systemctl daemon-reload"
-  action :run
+  action :nothing
 end
 
 service 'tomcat' do
